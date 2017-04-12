0978a920424287e9d23df15a51ae0a2b2f5a8c9b
account931 <account931@ukr.net>
2017-04-12 10:40:42 +0300

    Add files via upload

---
+++ script/script-move.js
@@ -0,0 +1,87 @@
+$(document).ready(function(){
+ 
+
+
+
+
+// **************************************************************************************
+// **************************************************************************************
+//                                                                                     ** 
+//Changin   enemy  position on  windows  resize----------------------DELETE??????-
+$(window).resize(function() {
+
+      //var pos = $(".slide_circle1").position(); // returns an object with the attribute top and left   //$(selector).offset()
+var posoffset = $("#ballS").offset();  //alert(posoffset.top);
+//var x=pos.top;  // top offset position
+//var y=pos.left; // left offset position
+
+$(".enemy").offset({top:posoffset.top,left:posoffset.left});
+
+});
+
+// End  Changin   color  slide  cicrle  position------------------
+// **                                                                                  **
+// **************************************************************************************
+// **************************************************************************************
+
+
+
+
+
+
+
+
+// **************************************************************************************
+// **************************************************************************************
+//                                                                                     ** 
+// Reset   enemies   while  touching  the  slider -------------------------------------
+    $("#slider").click(function(){
+                         var posoffset = $("#ballS").offset();  //alert(posoffset.top);
+                         $(".enemy").offset({top:posoffset.top,left:posoffset.left});      
+    });
+// End Reset   enemies   while  touching  the  slider ---------------------------------
+
+// **                                                                                  **
+// **************************************************************************************
+// **************************************************************************************
+
+
+
+
+
+
+//end  ready
+});
+
+
+
+
+
+
+// **************************************************************************************
+// **************************************************************************************
+//                                                                                     ** 
+//function  that chanes  enemy  position while target is  moved
+function myfuctXXX(){
+var posoffset = $("#ballS").offset();  //alert(posoffset.top);
+//$(".enemy").offset({top:posoffset.top,left:posoffset.left});  alert(leftX);
+$(".enemy").offset({top:topX,left:leftX});  //alert(topX+'---'+leftX);
+
+// alert($(".enemy").length);
+
+  //running  for  loop  for  every  .enemy  class  and  change each position
+  for (i = 0; i < $(".enemy").length; i++)
+  {
+          topX=parseInt(topX)+(i*26); //alert(topX); //make  every  enemy  move  to  diffrent  coordinates
+          leftX=parseInt(leftX)+(i*26);
+  $(".enemy").eq(i).animate({  //eq(i)==[i]
+          top: topX,left:leftX
+       /* top: "-=30px",left:"99px;"*/
+                            }, 1000-300);
+
+  } //  end  for 
+}//end  function
+// **                                                                                  **
+// **************************************************************************************
+// **************************************************************************************
+
